589c4754679c94c202b8f4e74622e7a9cc91cd05
tamamiyasita <46100058+tamamiyasita@users.noreply.github.com>
2021-02-14 01:36:31 +0900

    tkm

--- attack_effect.py
+++ attack_effect.py
@@ -11,20 +11,20 @@ class AttackEffect:
         self.step = GRID_SIZE // 2
 
 
-    def start(self, actor, attack_target, from_x, from_y, attack_target_x):
+    def start(self, owner, attack_target, from_x, from_y, attack_target_x):
         attack_target.state = state.DEFENSE
         
 
-        if abs(from_x - actor.center_x) >= self.step and actor.dx or\
-                abs(from_y - actor.center_y) >= self.step and actor.dy:
-            actor.change_x = 0
-            actor.change_y = 0
+        if abs(from_x - owner.center_x) >= self.step and owner.dx or\
+                abs(from_y - owner.center_y) >= self.step and owner.dy:
+            owner.change_x = 0
+            owner.change_y = 0
 
         if self.attack_delay == 6:
             for i in range(PARTICLE_COUNT):
                 particle = AttackParticle()
                 particle.position = (
-                    actor.center_x + (actor.dx*20), actor.center_y + (actor.dy*20))
+                    owner.center_x + (owner.dx*20), owner.center_y + (owner.dy*20))
                 self.tmp_effect_sprites.append(particle)
                 attack_target.change_x += uniform(-0.7, 0.7)
 
@@ -34,17 +34,15 @@ class AttackEffect:
         else:
             attack_target.alpha = 155
 
-        if actor.change_x == 0 and actor.change_y == 0 and actor.state != state.TURN_END:
+        if owner.change_x == 0 and owner.change_y == 0 and owner.state != state.TURN_END:
             self.attack_delay -= 1
 
             if 0 > self.attack_delay:
-                actor.center_y = actor.from_y
-                actor.center_x = actor.from_x
-                actor.change_x, actor.change_y = 0, 0
+                owner.center_y = owner.from_y
+                owner.center_x = owner.from_x
+                owner.change_x, owner.change_y = 0, 0
                 attack_target.alpha = 255
                 attack_target.change_x = 0
                 attack_target.center_x = attack_target_x
-                actor.state = state.TURN_END
-
-        if actor.state == state.TURN_END:
-            actor.wait = actor.fighter.attack_speed
+                owner.wait = owner.fighter.attack_speed
+                owner.state = state.TURN_END
